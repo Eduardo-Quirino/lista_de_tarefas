412f7bdf857930dfacb1f1ce6365d7b9bdffc860
Eduardo Quirino <edu.3532@gmail.com>
2021-11-15 21:08:56 -0300

    Atualizando

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, {useState} from 'react';
 import "./App.css";
 
-const App = () => {
-  const [message, setMessage] = useState("Sejá bem vindo!");
 
-  return(
-   
-    <div>
-      <div className= "container">{message}</div>;
-      <button onClick={() => setMessage("Olá usuário")}>
-        Mudar mensagem
-      </button>
-    </div>
+const App = () => {
+  const [message, setMessage] = useState('Olá Usuário!');
+
+  return (
+      <div>
+
+        <div className = "container">{message}</div>
+        <button onClick ={() =>setMessage ("Sejá bem vindo!")}>Mudar mensagem</button>
+      </div>
   );
 };
  
